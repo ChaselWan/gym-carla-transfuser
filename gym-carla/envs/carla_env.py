51b089f90ebc93a155572a7aa7938a980c171adc
ChaselWan <Chasel_Wan@nuaa.edu.cn>
2023-06-05 15:03:34 +0800

    Create carla_env.py

---
+++ gym-carla/envs/carla_env.py
@@ -0,0 +1,258 @@
+#copyright cjy1992
+
+from carla.controller import PIDLongitudinalController
+import carla
+
+
+Class CarlaEnv(gym.Env):
+  """Modified Gym-Carla-Env"""
+  def __init__(self,params):
+    # Parameters
+    self.display_size = params['display_size']
+    self.max_past_step = params['max_past_step']
+    self.number_of_vehicles = params['number_of_vehicles']
+    self.number_of_walkers = params['number_of_vehicles']
+    self.dt = params['dt']
+    self.task_mode = params['task_mode']
+    self.max_time_episode = params['max_time_episode']
+    self.max_waypt = params['max_waypt']
+    self.obs_range = params['obs_range']
+    self.lidar_bin = params['lidar_bin']
+    self.d_behind = params['d_behind']
+    self.obs_size = int(self.obs_range/self.lidar_bin)
+    self.out_lane_thres = params['out_lane_thres']
+    self.desired_speed = params['desired_speed']
+    self.max_ego_spawn_times = params['max_ego_spawn_times']
+    self.display_route = params['display_route']
+    
+    # pixor is Always False,so Deleted
+    
+    # Destination
+    if params['task_mode'] == 'roundabout':
+      self.dests = [[4.46, -61.46, 0], [-49.53, -2.89, 0], [-6.48, 55.47, 0], [35.96, 3.33, 0]]
+    else:
+      self.dests = None
+      
+    # observation spaces
+    observation_space_dict = {
+      'camera': spaces.Box(low=0, high=255, shape=(self.obs_size, self.obs_size, 3), dtype=np.uint8),
+      'lidar': spaces.Box(low=0, high=255, shape=(self.obs_size, self.obs_size, 3), dtype=np.uint8),
+      'birdeye': spaces.Box(low=0, high=255, shape=(self.obs_size, self.obs_size, 3), dtype=np.uint8),
+      'state': spaces.Box(np.array([-2, -1, -5, 0]), np.array([2, 1, 30, 1]), dtype=np.float32)
+      }
+    self.observation_space = spaces.Dict(observation_space_dict)
+    
+    # ego_vehicle takes action controlled by pid
+    # How to wrapper pid action in env?
+    # action can be wrappered by list[steer,throttle,brake]，But action can't be chosen randomly,instead of being controlled by pid.
+    self.action_space = [[],[]]
+    
+    
+    # Connect to carla server and get world object
+    print('connecting to Carla server...')
+    client = carla.Client('localhost', params['port'])
+    client.set_timeout(10.0)
+    self.world = client.load_world(params['town'])
+    print('Carla server connected!')
+    
+    # Set weather
+    self.world.set_weather(carla.WeatherParameters.ClearNoon)
+
+    # Get spawn points
+    self.vehicle_spawn_points = list(self.world.get_map().get_spawn_points())
+    self.walker_spawn_points = []
+    for i in range(self.number_of_walkers):
+      spawn_point = carla.Transform()
+      loc = self.world.get_random_location_from_navigation()
+      if (loc != None):
+        spawn_point.location = loc
+        self.walker_spawn_points.append(spawn_point)
+        
+    # Create the ego vehicle blueprint
+    self.ego_bp = self._create_vehicle_bluepprint(params['ego_vehicle_filter'], color='49,8,8')
+
+    # Collision sensor
+    self.collision_hist = [] # The collision history
+    self.collision_hist_l = 1 # collision history length
+    self.collision_bp = self.world.get_blueprint_library().find('sensor.other.collision')
+
+    # Lidar sensor
+    self.lidar_data = None
+    self.lidar_height = 2.1
+    self.lidar_trans = carla.Transform(carla.Location(x=0.0, z=self.lidar_height))
+    self.lidar_bp = self.world.get_blueprint_library().find('sensor.lidar.ray_cast')
+    self.lidar_bp.set_attribute('channels', '32')
+    self.lidar_bp.set_attribute('range', '5000')
+
+    # Camera sensor
+    self.camera_img = np.zeros((self.obs_size, self.obs_size, 3), dtype=np.uint8)
+    self.camera_trans = carla.Transform(carla.Location(x=0.8, z=1.7))
+    self.camera_bp = self.world.get_blueprint_library().find('sensor.camera.rgb')
+    # Modify the attributes of the blueprint to set image resolution and field of view.
+    self.camera_bp.set_attribute('image_size_x', str(self.obs_size))
+    self.camera_bp.set_attribute('image_size_y', str(self.obs_size))
+    self.camera_bp.set_attribute('fov', '110')
+    # Set the time in seconds between sensor captures
+    self.camera_bp.set_attribute('sensor_tick', '0.02')
+
+    # Set fixed simulation step for synchronous mode
+    self.settings = self.world.get_settings()
+    self.settings.fixed_delta_seconds = self.dt
+
+    # Record the time of total steps and resetting steps
+    self.reset_step = 0
+    self.total_step = 0
+    
+    # Initialize the renderer
+    self._init_renderer()
+
+    #pixor deleted
+    
+  def reset(self):
+    # Clear sensor objects
+    self.collision_sensor = None
+    self.lidar_sensor = None
+    self.camera_sensor = None
+    
+    # Delete sensors, vehicles and walkers
+    self._clear_all_actors(['sensor.other.collision', 'sensor.lidar.ray_cast', 'sensor.camera.rgb', 'vehicle.*', 'controller.ai.walker', 'walker.*'])
+    
+    # Disabled sync mode
+    self._set_synchronous_mode(False)
+      
+    # Spawn surrounding vehicles
+    random.shuffle(self.vehicle_spawn_points)
+    count = self.number_of_vehicles:
+      if count > 0:
+        for spawn_point in self.vehicle_spawn_points:
+          if self._try_spawn_random_vehicle_at(spawn_point, number_of_wheels=[4]):
+            count -= 1
+          if count <= 0:
+            break
+      while count > 0:
+        if self._try_spawn_random_vehicle_at(random.choice(self.vehicle_spawn_points), number_of_wheels=[4]):
+          count -= 1
+          
+    # Spawn pedestrians
+    random.shuffle(self.walker_spawn_points)
+    count = self.number_of_walkers
+    if count > 0:
+      for spawn_point in self.walker_spawn_points:
+        if self._try_spawn_random_walker_at(spawn_point):
+          count -= 1
+        if count <= 0:
+          break
+    while count > 0:
+      if self._try_spawn_random_walker_at(random.choice(self.walker_spawn_points)):
+        count -= 1
+
+      if self._try_spawn_random_walker_at(random.choice(self.walker_spawn_points)):
+        count -= 1
+        
+# Get actors polygon list
+    self.vehicle_polygons = []
+    vehicle_poly_dict = self._get_actor_polygons('vehicle.*')
+    self.vehicle_polygons.append(vehicle_poly_dict)
+    self.walker_polygons = []
+    walker_poly_dict = self._get_actor_polygons('walker.*')
+    self.walker_polygons.append(walker_poly_dict)
+
+    # Spawn the ego vehicle
+    ego_spawn_times = 0
+    while True:
+      if ego_spawn_times > self.max_ego_spawn_times:
+        self.reset()
+
+      if self.task_mode == 'random':
+        transform = random.choice(self.vehicle_spawn_points)
+      if self.task_mode == 'roundabout':
+        self.start=[52.1+np.random.uniform(-5,5),-4.2, 178.66] # random
+        # self.start=[52.1,-4.2, 178.66] # static
+        transform = set_carla_transform(self.start)
+      if self._try_spawn_ego_vehicle_at(transform):
+        break
+      else:
+        ego_spawn_times += 1
+        time.sleep(0.1)    
+
+    # Add collision sensor
+    self.collision_sensor = self.world.spawn_actor(self.collision_bp, carla.Transform(), attach_to=self.ego)
+    self.collision_sensor.listen(lambda event: get_collision_hist(event))
+    def get_collision_hist(event):
+      impulse = event.normal_impulse
+      intensity = np.sqrt(impulse.x**2 + impulse.y**2 + impulse.z**2)
+      self.collision_hist.append(intensity)
+      if len(self.collision_hist)>self.collision_hist_l:
+        self.collision_hist.pop(0)
+    self.collision_hist = []
+    
+     # Add lidar sensor
+    self.lidar_sensor = self.world.spawn_actor(self.lidar_bp, self.lidar_trans, attach_to=self.ego)
+    self.lidar_sensor.listen(lambda data: get_lidar_data(data))
+    def get_lidar_data(data):
+      self.lidar_data = data
+
+    # Add camera sensor
+    self.camera_sensor = self.world.spawn_actor(self.camera_bp, self.camera_trans, attach_to=self.ego)
+    self.camera_sensor.listen(lambda data: get_camera_img(data))
+    def get_camera_img(data):
+      array = np.frombuffer(data.raw_data, dtype = np.dtype("uint8"))
+      array = np.reshape(array, (data.height, data.width, 4))
+      array = array[:, :, :3]
+      array = array[:, :, ::-1]
+      self.camera_img = array
+
+    # Update timesteps
+    self.time_step=0
+    self.reset_step+=1
+
+    # Enable sync mode
+    self.settings.synchronous_mode = True
+    self.world.apply_settings(self.settings)
+   
+  def step(self, action):
+    # Calculate action
+    steer, throttle, brake = self.nets[0].control_pid(self.pred_wp, gt_velocity, is_stuck)  # be wrappered
+    
+    
+    
+    def control_pid(self, waypoints, velocity, is_stuck):
+        ''' Predicts vehicle control with a PID controller.
+        Args:
+            waypoints (tensor): output of self.plan()
+            velocity (tensor): speedometer input
+        '''
+        assert(waypoints.size(0)==1)
+        waypoints = waypoints[0].data.cpu().numpy()
+        # when training we transform the waypoints to lidar coordinate, so we need to change is back when control
+        waypoints[:, 0] += self.config.lidar_pos[0]
+
+        speed = velocity[0].data.cpu().numpy()
+
+        desired_speed = np.linalg.norm(waypoints[0] - waypoints[1]) * 2.0
+
+        if is_stuck:
+            desired_speed = np.array(self.config.default_speed) # default speed of 14.4 km/h
+
+        brake = ((desired_speed < self.config.brake_speed) or ((speed / desired_speed) > self.config.brake_ratio))
+
+        delta = np.clip(desired_speed - speed, 0.0, self.config.clip_delta)
+        throttle = self.speed_controller.step(delta)
+        throttle = np.clip(throttle, 0.0, self.config.clip_throttle)
+        throttle = throttle if not brake else 0.0
+        aim = (waypoints[1] + waypoints[0]) / 2.0
+        angle = np.degrees(np.arctan2(aim[1], aim[0])) / 90.0
+        if (speed < 0.01):
+            angle = 0.0  # When we don't move we don't want the angle error to accumulate in the integral
+        if brake:
+            angle = 0.0
+        
+        steer = self.turn_controller.step(angle)
+
+        steer = np.clip(steer, -1.0, 1.0) #Valid steering values are in [-1,1]
+
+        return steer, throttle, brake    
+    
+    
+    
+    
